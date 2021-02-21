49bfc8aa113b0f33688c23dd2e5582ea7a613d91
chairielazizi <azizihr2000@gmail.com>
2021-02-21 16:45:41 +0800

    create the request fx to generate the shortened link, but not appearing in the django admin db

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+import uuid
+from .models import Url
+from django.http import HttpResponse
 
 # Create the views ,and url mapping
 def index(request):
     return render(request, 'index.html')
 
 def create(request):
-    pass
+    # pass
+    if request.method == 'POST':
+        link = request.POST['link'] #the id link from html file
+        uid = str(uuid.uuid4())[:5] #shorten to only 5 character
+        new_url = Url(link=link,uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
