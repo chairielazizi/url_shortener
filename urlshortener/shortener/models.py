bb71bd88672fbcb312dbfaf9e484ba9513278eb6
chairielazizi <azizihr2000@gmail.com>
2021-02-21 16:27:11 +0800

    set the form function using ajax

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. chairiel,huko
 class Url(models.Model):
     link = models.CharField(max_length=10000)
     uuid = models.CharField(max_length=10)
+
--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create the views ,and url mapping
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    pass
